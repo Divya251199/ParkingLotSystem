198614969f05ae868fdf0eda50dfd35f02e498cc
Divya251199 <92331091+Divya251199@users.noreply.github.com>
2023-03-12 20:56:17 +0530

    Update Main.java

--- ParkingLot/Main.java
+++ ParkingLot/Main.java
@@ -2,8 +2,14 @@ public class Main {
     public static void main(String[] args) {
         ParkingLot parkingLot = new ParkingLot();
         parkingLot.initSlots();
-        Car c1 = new car("DL6A5757", "Nikhil");
+        Car c1 = new Car("DL6A5757", "Nikhil");
         Ticket t1 = parkingLot.park(c1);
         System.out.println(t1);
+        try {
+            Thread.sleep(3000);
+        } catch(Exception e) {
+            
+        }
+        System.out.println(parkingLot.unpark(t1));
     }
 }
