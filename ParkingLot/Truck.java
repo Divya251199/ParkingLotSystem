07a6eac407e0e476d486d85549410b645fb04bdf
Divya251199 <92331091+Divya251199@users.noreply.github.com>
2023-03-12 20:59:03 +0530

    Update Truck.java

--- ParkingLot/Truck.java
+++ ParkingLot/Truck.java
@@ -1,6 +1,6 @@
 public class Truck extends Vehicle {
     public Truck(String number, String ownerName) {
         super(number, ownerName);
-        this.parkingPrice = 40;
+        super.parkingPrice = 40;
     }
 }
